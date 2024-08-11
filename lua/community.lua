b3e2ad68492029fe76496e28adf33b830f737622
Jonathan Irhodia <jonathanirhodia@gmail.com>
2024-08-11 11:17:51 +0100

    Add colorschemes and language packs

--- lua/community.lua
+++ lua/community.lua
@@ -9,4 +9,18 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- color schemes
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nord" },
+  -- language packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.pack.ocaml" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.sql" },
 }
